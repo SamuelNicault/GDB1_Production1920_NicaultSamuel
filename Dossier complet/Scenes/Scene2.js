dfccb4e6dd3035206c8c5df81ba537958e2d7263
NicaultSamuel <nicault.samuel@gmail.com>
2020-06-03 17:14:28 +0200

    Fin du second Jeu

--- Dossier complet/Scenes/Scene2.js
+++ Dossier complet/Scenes/Scene2.js
@@ -11,10 +11,12 @@ class Scene2 extends Phaser.Scene {
 
     this.quete;
     this.press;
-    this.niveau = 1;
+    this.niveau = 6;
+    this.cible;
   }
 
  preload() {
+          this.load.image("cible", "assets/t_cible.png");
           this.load.image("timebar", "assets/timer.png");
           this.load.image("viseur", "assets/t_viseur.png");
           this.load.image("centre", "assets/t_centre_viseur.png");
@@ -28,11 +30,12 @@ class Scene2 extends Phaser.Scene {
     }
 
     create() {
-            this.diff = 1 * this.niveau;
-          
+            this.diff = 400 / (0.5*this.niveau);
+            this.Y = Phaser.Math.Between(50,100);
+            this.Y1 = Phaser.Math.Between(-10,80);
+            this.X = Phaser.Math.Between(30,60);
 
             this.fond = this.add.image(0,0,'entier').setOrigin(0,0);
-
                         
             this.viseur = this.physics.add.image(374,188,'viseur');
             this.viseur.setCollideWorldBounds(true);
@@ -47,69 +50,100 @@ class Scene2 extends Phaser.Scene {
             this.flecheG = this.add.image(54,315,'flecheG').setInteractive();
             this.flecheD = this.add.image(106,315,'flecheD').setInteractive();
 
-            //this.camera2.setAlpha(0.1).setBounds(0, 0, 748, 376).setBackgroundColor(0x000).ignore(this.zoneTap,this.croix).ignore(this.croix).ignore(this.flecheH).ignore(this.flecheB).ignore(this.flecheG).ignore(this.flecheD);
 
+            this.cibles = this.physics.add.group({
+              key: 'cible',
+              repeat: 3,
+              setXY: {x: 200, y: this.Y, stepX: 100, stepY: this.Y1}
+            });
+            this.cibles.children.iterate(function (cible) {
+              cible.setGravityY(0);
+            });
 
+            this.victoireText = this.add.text(284, 176, "MEDAILLE D'OR", {'font': '23px', fill: '#fff'});
+            this.victoireText.visible = false;
 
-            this.camera1.setAlpha(1).startFollow(this.viseur).setBounds(0, 0, 748, 376).ignore(this.zoneTap,this.croix).ignore(this.croix).ignore(this.flecheH).ignore(this.flecheB).ignore(this.flecheG).ignore(this.flecheD);
-            
-            this.camera2.setAlpha(1).setBounds(0, 0, 748, 376).setBackgroundColor(0x000).ignore(this.centre).ignore(this.viseur).ignore(this.fond);
+            this.argentText = this.add.text(254, 176, "MEDAILLE D'ARGENT", {'font': '23px', fill: '#fff'});
+            this.argentText.visible = false;
 
-            this.camera3.setAlpha(0.1).setBounds(0, 0, 748, 376).setBackgroundColor(0x000).ignore(this.zoneTap,this.croix).ignore(this.croix).ignore(this.flecheH).ignore(this.flecheB).ignore(this.flecheG).ignore(this.flecheD);
+            this.bronzeText = this.add.text(254, 176, "MEDAILLE DE BRONZE", {'font': '23px', fill: '#fff'});
+            this.bronzeText.visible = false;
 
+            this.perduText = this.add.text(324, 176, "HONTEUX", {'font': '23px', fill: '#fff'});
+            this.perduText.visible = false;
 
-            this.zoneTap.on('pointerdown',() => {
-              console.log("hello");
-            })
+            this.camera1.setAlpha(1).startFollow(this.viseur).setBounds(0, 0, 748, 376).ignore(this.zoneTap).ignore(this.croix).ignore(this.flecheH).ignore(this.flecheB).ignore(this.flecheG).ignore(this.flecheD);
+            this.camera2.setAlpha(1).setBounds(0, 0, 748, 376).setBackgroundColor(0x000).ignore(this.centre).ignore(this.cibles).ignore(this.viseur).ignore(this.fond);
+            this.camera3.setBounds(0, 0, 748, 376).setBackgroundColor(0x000).ignore(this.zoneTap,this.croix).ignore(this.cibles).ignore(this.croix).ignore(this.flecheH).ignore(this.flecheB).ignore(this.flecheG).ignore(this.flecheD);
+
+            this.physics.add.overlap(this.centre, this.cibles, shoot, null,this);
+
+            function shoot(centre, cible){
+              this.zoneTap.on('pointerdown',() => {
+                cible.disableBody(true,true);
+                if (this.isWin == 1){
+                  this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+                }
+              })
+            };
+
+            this.isWin = 0;
 
 
             this.flecheH.on('pointerdown',() => {
-              this.viseur.setVelocityY(-200); 
-              this.centre.setVelocityY(-200); 
-              console.log("hello");
+              this.viseur.setVelocityY(-this.diff); 
+              this.centre.setVelocityY(-this.diff);
+              if (this.isWin == 1){
+              this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+            } 
             })
             this.flecheH.on('pointerup',() => {
               this.viseur.setVelocityY(0); 
               this.centre.setVelocityY(0); 
-              console.log("hello");
             })
 
 
             this.flecheB.on('pointerdown',() => {
-              this.viseur.setVelocityY(200);
-              this.centre.setVelocityY(200);
-              console.log("hello");
+              this.viseur.setVelocityY(this.diff);
+              this.centre.setVelocityY(this.diff);
+              if (this.isWin == 1){
+              this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+            }
             })
             this.flecheB.on('pointerup',() => {
               this.viseur.setVelocityY(0);
               this.centre.setVelocityY(0);
-              console.log("hello");
             })
 
 
             this.flecheG.on('pointerdown',() => {
-              this.viseur.setVelocityX(-200);
-              this.centre.setVelocityX(-200);
-              console.log("hello");
+              this.viseur.setVelocityX(-this.diff);
+              this.centre.setVelocityX(-this.diff);
+              if (this.isWin == 1){
+              this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+            }
             })
             this.flecheG.on('pointerup',() => {
               this.viseur.setVelocityX(0);
               this.centre.setVelocityX(0);
-              console.log("hello");
             })
 
 
             this.flecheD.on('pointerdown',() => {
-              this.viseur.setVelocityX(200);
-              this.centre.setVelocityX(200);
-              console.log("hello");
+              this.viseur.setVelocityX(this.diff);
+              this.centre.setVelocityX(this.diff);
+              if (this.isWin == 1){
+              this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+            }
             })
             this.flecheD.on('pointerup',() => {
               this.viseur.setVelocityX(0);
               this.centre.setVelocityX(0);
-              console.log("hello");
             })
 
+            function changeLevel () {
+              console.log("change de level");
+            }
             
 
         //Timer
@@ -143,17 +177,35 @@ class Scene2 extends Phaser.Scene {
                     let stepWidth = this.energyMask.displayWidth / gameOptions.initialTime;
                     this.energyMask.x -= stepWidth;
                     if(this.timeLeft == 0){
-                        console.log("looser");
-                        //this.scene.start("PlayGame");
+                      if (this.cibles.countActive(true) === 1){
+                        this.argentText.visible = true;
+                        this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+                      }
+                      if (this.cibles.countActive(true) === 2){
+                        this.bronzeText.visible = true;
+                        this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+                      }
+                      if (this.cibles.countActive(true) === 3){
+                        this.perduText.visible = true;
+                        this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+                      }
+                      if (this.cibles.countActive(true) === 4){
+                        this.perduText.visible = true;
+                        this.timedEvent = this.time.delayedCall(4000, changeLevel, [], this);
+                      }
                     }
                 },
                 callbackScope: this,
                 loop: true
             });
+
     }
   update() {
-    if (this.viseur.x >= (631 + this.diff)){
+    if (this.cibles.countActive(true) === 0){
       this.gameTimer.paused = true;
+      this.victoireText.visible = true;
+      this.isWin = 1;
+      console.log(this.isWin);
     }
     
 
